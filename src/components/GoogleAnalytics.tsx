fa30273406083976e8883b7bcabedd3d00a50901
brunopoggi9-oss <brunopoggi9@gmail.com>
2026-07-12 10:42:15 +0200

    Create GoogleAnalytics.tsx

---
+++ src/components/GoogleAnalytics.tsx
@@ -0,0 +1,26 @@
+import Script from 'next/script';
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-CW08FD0Y27`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-CW08FD0Y27', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+    </>
+  );
+}
